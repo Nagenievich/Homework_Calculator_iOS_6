0e907a29ecbccdc4cab7cbbb73c789e318eaaf46
Alexey Makarov <alexeynkrv@gmail.com>
2021-02-03 15:57:03 +0700

    Initial Commit

---
+++ Calculator_iOS_6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator_iOS_6
+//
+//  Created by Alexey Makarov on 03.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
